ae058d5288c1327f077e14f6ffde2309679b7482
Tima <tima@Timas-MacBook-Pro.local>
2026-05-07 14:59:26 +0600

    fixedstash

--- src/middlewere/rateLimiter.js
+++ src/middlewere/rateLimiter.js
@@ -1,15 +1,20 @@
-import ratelimit from '../config/upStash.js';
+import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
-    try {
-  const { success } = await ratelimit.limit("my-rate-limit");
-        if (!success) {
-            return res.status(429).json({ message: "Too many requests" });
-        }
-        next();
-    } catch (error) {
-        console.log("Error in rate limiter", error);
-        next(error);
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
+
+    if (!success) {
+      return res.status(429).json({
+        message: "Too many requests, please try again later.",
+      });
     }
-}
+
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
 export default rateLimiter;
